556ee9ff1ad1fa749b53e131e0860baf23de36c6
Niraj142002 <niraj.patil16444@sakec.ac.in>
2024-07-16 09:02:26 +0000

    az

--- app.py
+++ app.py
@@ -1,3 +1,34 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c973022be376c788d0a6e49d72216e447e231612d528a1bdd8dd579d1fc06683
-size 1072
+from flask import Flask, request, render_template, jsonify
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from optimum.intel import OVModelForCausalLM
+import torch
+
+
+
+app = Flask(__name__)
+
+# Load the tokenizer and model from a local path
+model_name_or_path = r"/Users/niraj/Downloads/Intel/saved-base"
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    data = request.get_json()
+    text = data['text']
+    model_id = "./tinyllama-art-s-1"
+    ov_model = OVModelForCausalLM.from_pretrained("ov_model", use_cache=False)
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    inputs = tokenizer(text, return_tensors="pt")
+    outputs = ov_model.generate(**inputs)
+    summary = tokenizer.decode(outputs[0])
+
+    
+    return jsonify({'summary': summary})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- get-pip.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0f8bb2652c0b0965f268312f49ec21e772d421d381af4324beea66b8acf2635c
-size 2277800
